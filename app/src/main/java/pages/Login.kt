7911f6fab945fa62db2e2109b10c5542326ccbd9
Prajal Gupta <prajalgupta824@gmail.com>
2023-04-16 22:51:04 +0530

    facebook auth removed for now, error resolved

--- app/src/main/java/pages/Login.kt
+++ app/src/main/java/pages/Login.kt
@@ -8,46 +8,47 @@ import android.os.Handler
 import android.text.TextUtils
 import android.view.Window
 import android.widget.Button
-import android.widget.ImageView
+//import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+//import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.eveon.R
 import com.example.eveon.activitiesandfragments.MainActivity
-import com.example.eveon.activitiesandfragments.p_details
-import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
+//import com.example.eveon.activitiesandfragments.p_details
+//import com.facebook.AccessToken
+//import com.facebook.CallbackManager
+//import com.facebook.FacebookCallback
+//import com.facebook.FacebookException
+//import com.facebook.login.LoginManager
+//import com.facebook.login.LoginResult
+//import com.google.android.gms.auth.api.signin.GoogleSignIn
+//import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+//import com.google.android.gms.auth.api.signin.GoogleSignInClient
+//import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+//import com.google.android.gms.common.api.ApiException
 
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthCredential
-import com.google.firebase.auth.GoogleAuthProvider
+//import com.google.firebase.auth.GoogleAuthCredential
+//import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
     private lateinit var  mprogressdialog: Dialog
     private lateinit var mauth:FirebaseAuth
-     private lateinit var googlesigninclient:GoogleSignInClient
+//     private lateinit var googlesigninclient:GoogleSignInClient
 //     private lateinit var facebookbutton:Button
 //     private lateinit var callbackmanager:CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    mauth= FirebaseAuth.getInstance()
         val btnsignup=findViewById<TextView>(R.id.gotoRegister)
 //        facebookbutton=findViewById(R.id.facebookLogin)
         btnsignup.setOnClickListener {
             startActivity(Intent(this,SignUp::class.java))
         }
-      mauth= FirebaseAuth.getInstance()
+
 //        callbackmanager= CallbackManager.Factory.create()
 //        val accessToken=AccessToken.getCurrentAccessToken()
 //        if(accessToken!=null && !accessToken.isExpired)
@@ -76,14 +77,14 @@ class Login : AppCompatActivity() {
         findViewById<Button>(R.id.lgnButton).setOnClickListener{
            signinregistereduser()
     }
-        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
-        requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
-
-        googlesigninclient= GoogleSignIn.getClient(this,gso)
-        val googlelogin=findViewById<ImageView>(R.id.googleLogin).setOnClickListener {
-            showprogressdialog("Signing Up...")
-            SigninGoogle()
-        }
+//        val gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+//        requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+//
+//        googlesigninclient= GoogleSignIn.getClient(this,gso)
+//        val googlelogin=findViewById<ImageView>(R.id.googleLogin).setOnClickListener {
+//            showprogressdialog("Signing Up...")
+//            SigninGoogle()
+//        }
         val forgotpassword=findViewById<TextView>(R.id.forgotPassword).setOnClickListener {
             val intent =Intent(this,forgotpassword::class.java)
             startActivity(intent)
@@ -98,57 +99,57 @@ class Login : AppCompatActivity() {
 
     }
 
-    private fun SigninGoogle() {
-
-        val signinintent=googlesigninclient.signInIntent
-        startActivityForResult(signinintent,100)
-
-
-
-
-
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==100)
-        {
-            val task=GoogleSignIn.getSignedInAccountFromIntent(data)
-            val account=task.getResult(ApiException::class.java)
-            val credential=GoogleAuthProvider.getCredential(account.idToken,null)
-            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this) {
-            task->hideprogressdialog()
-                if(task.isSuccessful)
-                {
+//    private fun SigninGoogle() {
+//
+//        val signinintent=googlesigninclient.signInIntent
+//        startActivityForResult(signinintent,100)
+//
+//
+//
+//
+//
+//    }
 
 
-                    val layout123=layoutInflater.inflate(R.layout.custom_toast_layout,findViewById(R.id.view_layout_of_toast))
-                    val   toast4: Toast = Toast(this)
-                    toast4.view=layout123
-                    val txtmsg12:TextView=layout123.findViewById(R.id.textview_toast)
-                    txtmsg12.setText( "You Have Signed In Successfully")
-                    toast4.duration.toLong()
-                    toast4.show()
-                    val intent=Intent(this,MainActivity::class.java)
-                    startActivity(intent)
-
-
-                }
-                else{
-
-                    val layout1=layoutInflater.inflate(R.layout.error_toast_layout,findViewById(R.id.view_layout_of_toast1))
-                    val toast1:Toast= Toast(this)
-                    toast1.view=layout1
-                    val txtmsg1:TextView=layout1.findViewById(R.id.textview_toast1)
-                    txtmsg1.setText(task.exception!!.message)
-                    toast1.duration.toShort()
-                    toast1.show()
-
-                }
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if(requestCode==100)
+//        {
+//            val task=GoogleSignIn.getSignedInAccountFromIntent(data)
+//            val account=task.getResult(ApiException::class.java)
+//            val credential=GoogleAuthProvider.getCredential(account.idToken,null)
+//            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(this) {
+//            task->hideprogressdialog()
+//                if(task.isSuccessful)
+//                {
+//
+//
+//                    val layout123=layoutInflater.inflate(R.layout.custom_toast_layout,findViewById(R.id.view_layout_of_toast))
+//                    val   toast4: Toast = Toast(this)
+//                    toast4.view=layout123
+//                    val txtmsg12:TextView=layout123.findViewById(R.id.textview_toast)
+//                    txtmsg12.setText( "You Have Signed In Successfully")
+//                    toast4.duration.toLong()
+//                    toast4.show()
+//                    val intent=Intent(this,MainActivity::class.java)
+//                    startActivity(intent)
+//
+//
+//                }
+//                else{
+//
+//                    val layout1=layoutInflater.inflate(R.layout.error_toast_layout,findViewById(R.id.view_layout_of_toast1))
+//                    val toast1:Toast= Toast(this)
+//                    toast1.view=layout1
+//                    val txtmsg1:TextView=layout1.findViewById(R.id.textview_toast1)
+//                    txtmsg1.setText(task.exception!!.message)
+//                    toast1.duration.toShort()
+//                    toast1.show()
+//
+//                }
+//            }
+//        }
+//    }
 
     private fun signinregistereduser() {
         val email1:String = findViewById<TextView>(R.id.inputEmail).text.toString().trim{it<= ' ' }
